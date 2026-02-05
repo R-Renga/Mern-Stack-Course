d59be83cdefc426842cd03c63ef1e06dad1428f8
RengaRaja R <84700157+R-Renga@users.noreply.github.com>
2026-02-05 08:56:14 +0530

    Update longestrepeating.js

--- DSA/String/longestrepeating.js
+++ DSA/String/longestrepeating.js
@@ -21,3 +21,30 @@ function sub(s) {
 
 const result = sub("abccwopo");
 console.log(result);
+
+
+
+function sub(s) {
+   let i = 0;
+   let map = {};
+   let maxLen = 0;
+   let start = 0; // start index of longest substring
+
+   for (let j = 0; j < s.length; j++) {
+      if (map[s[j]] !== undefined && map[s[j]] >= i) {
+         i = map[s[j]] + 1;
+      }
+
+      map[s[j]] = j;
+
+      if (j - i + 1 > maxLen) {
+         maxLen = j - i + 1;
+         start = i;
+      }
+   }
+
+   return s.substring(start, start + maxLen);
+}
+
+const results = sub("abccwopo");
+console.log(results);
